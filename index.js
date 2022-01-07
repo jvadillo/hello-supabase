52ba3e60db40f972389a511384dbc38e98edcbac
Jon Vadillo <vadillo.jon@gmail.com>
2022-01-07 08:24:46 +0100

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTUwOTI2NiwiZXhwIjoxOTU3MDg1MjY2fQ.airoE5CDoDD1puSdESDXj_axMhagx2FucPk2BIShuFc'
 const SUPABASE_URL = "https://gqpoedayztfotgcdrhua.supabase.co"
