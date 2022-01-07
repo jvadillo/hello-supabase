efd57113b28fb2c63ca34ca0feaf84e0a2f22d88
Jon Vadillo <vadillo.jon@gmail.com>
2022-01-07 08:09:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js'
+
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTUwOTI2NiwiZXhwIjoxOTU3MDg1MjY2fQ.airoE5CDoDD1puSdESDXj_axMhagx2FucPk2BIShuFc'
+const SUPABASE_URL = "https://gqpoedayztfotgcdrhua.supabase.co"
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function leerRecursos() {
+
+    let { data: recursos, error } = await supabase
+    .from('recursos')
+    .select('id')
+
+    console.log(data);
+
+}
+
+leerRecursos();
+
