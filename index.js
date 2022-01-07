8dd079b953d44a02704a35255233bebb2fce7fa8
Jon Vadillo <vadillo.jon@gmail.com>
2022-01-07 08:36:57 +0100

    new try for loading createclient

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const { createClient } = supabase
-
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTUwOTI2NiwiZXhwIjoxOTU3MDg1MjY2fQ.airoE5CDoDD1puSdESDXj_axMhagx2FucPk2BIShuFc'
 const SUPABASE_URL = "https://gqpoedayztfotgcdrhua.supabase.co"
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 async function leerRecursos() {
 
